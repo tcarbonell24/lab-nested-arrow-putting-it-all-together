12430035826a8d5d5c03f6aadebbc4cf6fcd58b8
Thomas <tcarbonell12@gmail.com>
2025-04-12 00:36:24 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+let userInfo = {
+  "username": "user1",
+  "password": "password123"
+}
 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  let passwordCheck = (passwordAttempt) => {
+    
+    
+    if (attemptCount >= 3){
+      return("Account locked due to too many failed login attempts");
+    }
+
+    if (passwordAttempt === userInfo.password) {
+      return("Login successful");
+
+    } else{
+      attemptCount ++;
+      return(`Attempt ${attemptCount}: Login failed`);
+    }
+  }
+
+  return passwordCheck;
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
